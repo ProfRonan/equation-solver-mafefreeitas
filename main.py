862ef20f422f63db84ff52793aa0ec51dbd116a0
mafefreeitas <128387890+mafefreeitas@users.noreply.github.com>
2023-05-04 14:40:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-g=int(input("digite o grau da equaçao:"))
-if g <1 or g >2:
-    print("grau invalido")
-else:
-    if g==1:
-        print("a equaçao é do primeiro grau")
-        a= float(input("digite o valo de a:"))
-        if a==0:
-            print("valor de a inválido.")
+grau_equacao=float(input('qual o grau da equação?'))
+if grau_equacao < 1 or grau_equacao > 2:
+    print('Grau inválido')
+elif grau_equacao==1:
+    print('A equação é do primeiro grau')
+    a=float(input('digite o valor de a:'))
+    if a==0:
+        print('Valor de a inválido.')
+    else:
+        b=float(input('digite o valor de b:'))
+        raiz_grau1=-(b)/a
+        print(f'a raiz é {raiz_grau1:,.2f}')
+if grau_equacao==2:
+    print('A equação é do segundo grau.')
+    a=float(input('digite o valor de a:'))
+    if a ==0:
+        print('Valor de a inválido')
+    else:
+        b=float(input('digite o valor de b:'))
+        c=float(input('digite o valor de c:'))
+        delta=b**2-4*a*c
+        if delta <0:
+            print('A equação não possui raízes reais.')
+        elif delta ==0:
+            print('A equação possui um raíz real.')
+            raiz_real=-(b)/2*a
+            print(f'A equação possui apenas uma raíz real{raiz_real:,.2f}')
         else:
-            b=float(input("digite o valo de b:"))
-            x=-b/a
-            print("o valor da raiz é: {:.2f}".format(x))
-    else: 
-        print("a equaçao é do segundo grau.")
-        a=float(input("digite o valor de a:"))
-        if a==0:
-            print("valor de a inválido.")
-        else:
-            b=float(input("digite o valor de b:"))
-            c=float(input("digite o valor de c:"))
-            delta=b**2-4*a*c
-            if delta < 0:
-              print("nao exitem raixes reais para a equação")
-            elif delta==0:
-                x= -b/2*a
-                print("a equaçao so tem uma raiz real: {:.2f}".format(x))
+            print('A equação possui duas raizes reais')
+            reaiz_r1=(-b-(delta**0.5))/(2*a)
+            raiz_r2=(-b+(delta**0.5))/(2*a)
+            if raiz_r1 < raiz_r2:
+                print(f'A raiz real 1 é{raiz_r1:,.2f} e a raiz real 2 é {raiz_r2:,.2f}')
             else:
-             x1 = (-b + math.sqrt(delta))/(2*a)
-             x2 = (-b - math.sqrt(delta))/(2*a)
-            print("A equação possui duas raízes reais: {:.2f} e {:.2f}".format(min(x1,x2), max(x1,x2)))
+                print(f'a raiz real 1 é {raiz_r2:,.2f} e a raiz real 2 é{raiz_r1:,.2f}')
+        
